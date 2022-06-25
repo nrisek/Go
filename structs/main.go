8bb56abf1dd17367c8e97fbed23c3f58f0992008
Nikola Risek <nikolarisek001@gmail.com>
2022-06-25 15:27:33 +0200

    Created structs folder and new file main.go
    - learning Go structs

---
+++ structs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "alex@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	pointerAlex := &alex
+	pointerAlex.updateName("Alexy")
+	alex.print()
+}
+
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}

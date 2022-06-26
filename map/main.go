98255ff7bc26ef224e9973c0d01eae177687a1b9
Nikola Risek <nikolarisek001@gmail.com>
2022-06-26 11:52:25 +0200

    Implemented map in Go

---
+++ map/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4f879h",
+		"white": "#ffffff",
+	}
+
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, ": ", hex)
+	}
+}
